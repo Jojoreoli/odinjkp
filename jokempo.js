3fe4da223000c215334f216a257efa174993dc0e
João Pedro Reis de Oliveira <66749358+Jojoreoli@users.noreply.github.com>
2021-10-09 19:10:19 -0300

    WIP - User interface
    TO DO
      - Add responsivity
      - Add CSS hovers and stuff
      - Add High Score
      - Add confirm button (maybe?)

--- jokempo.js
+++ jokempo.js
@@ -1,70 +1,86 @@
 let selJogador, selPc;
-let pcWins = 0;
-let jogWins = 0;
+let pcWins = false;
+let jogWins = false;
+let previousChoice = null;
+let previousComputerChoice = null;
 
-function jogJoga(){
-	
-	return window.prompt("Make your play of rock, paper, scissors, lizard, spock...").toLowerCase();
-	
-
+function pcJoga() {
+  let escolhaPc;
+  let rng = Math.random();
+  if (rng < 0.2) {
+    escolhaPc = "rock";
+  } else if (rng > 0.2 && rng < 0.4) {
+    escolhaPc = "paper";
+  } else if (rng > 0.4 && rng < 0.6) {
+    escolhaPc = "scissors";
+  } else if (rng > 0.6 && rng < 0.8) {
+    escolhaPc = "lizard";
+  } else {
+    escolhaPc = "spock";
+  }
+	previousComputerChoice = escolhaPc;
+  return escolhaPc;
 }
 
-function pcJoga(){
-	let escolhaPc;
-	let rng = Math.random();
-	if (rng < 0.2){
-		escolhaPc = "rock";
-	} else if (rng > 0.2 && rng < 0.4){
-		escolhaPc = "paper";
-	} else if (rng > 0.4 && rng < 0.6){
-		escolhaPc = "scissors";
-	} else if (rng > 0.6 && rng < 0.8){
-		escolhaPc = "lizard";
-	} else {
-		escolhaPc = "spock";
-	}
-	
-	return escolhaPc;
+function Rodada() {
+  if (selJogador == "rock" && (selPc == "lizard" || selPc == "scissors") && selPc != "rock") {
+    jogWins = true;
+  } else if (selJogador == "paper" && (selPc == "spock" || selPc == "rock") && selPc != "paper") {
+    jogWins = true;
+  } else if (selJogador == "scissors" && (selPc == "lizard" || selPc == "paper") && selPc != "scissors") {
+    jogWins = true;
+  } else if (selJogador == "lizard" && (selPc == "spock" || selPc == "paper") && selPc != "lizard") {
+    jogWins = true;
+  } else if (selJogador == "spock" && (selPc == "scissors" || selPc == "rock") && selPc != "spock") {
+    jogWins = true;
+  } else if (selJogador == selPc) {
+		jogWins = false;
+		pcWins = false;
+	} else{
+    pcWins = true;
+  }
 }
 
-function Rodada(){
+function jogo(choice) {
+	// initial DOM Manipulation
+	const userChoice = document.querySelector(".userChoice");
+	const userChoiceIcon = document.querySelector(".userChoice__icon");
+	const computerChoice = document.querySelector(".computerChoice");
+	const computerChoiceIcon = document.querySelector(".computerChoice__icon");
+	userChoice.classList.contains("disabled") ? "" : userChoice.classList.add("disabled");
+	userChoiceIcon.classList.remove(`fa-hand-${previousChoice}`)
+	computerChoice.classList.contains("disabled") ? "" : computerChoice.classList.add("disabled");
+	computerChoiceIcon.classList.remove(`fa-hand-${previousComputerChoice}`);
+	const result = document.querySelector(".scoreArea__result");
 
-	if (selJogador == "rock" && (selPc == "lizard" || selPc == "scissors")){
-		console.log("The pitiful human wins!");
-		jogWins ++;
-	} else if (selJogador == "paper" && (selPc == "spock" || selPc == "rock")){
-		console.log("The pitiful human wins!");
-		jogWins ++;
-	} else if (selJogador == "scissors" && (selPc == "lizard" || selPc == "paper")){
-		console.log("The pitiful human wins!");
-		jogWins ++;
-	} else if (selJogador == "lizard" && (selPc == "spock" || selPc == "paper")){
-		console.log("The pitiful human wins!");
-		jogWins ++;
-	} else if (selJogador == "spock" && (selPc == "scissors" || selPc == "rock")){
-		console.log("The pitiful human wins!");
-		jogWins ++;
-	} else {
-		console.log("The superior machine wins!");
-		pcWins ++;
-	}
+	// Game Logic
+	jogWins = false;
+	pcWins = false;
+	selPc = pcJoga();
+	selJogador = choice;
+	Rodada();
+
+	console.log("jogador: ", jogWins, "pc: ", pcWins);
+
+	// final DOM Manipulation
+	userChoice.classList.remove("disabled");
+	userChoiceIcon.classList.add(`fa-hand-${choice}`);
+	computerChoice.classList.remove("disabled");
+	computerChoiceIcon.classList.add(`fa-hand-${selPc}`);
+
+
+	if(pcWins === false && jogWins === false) {
+		result.textContent = "It's a tie!";
+	} 
+  else if (pcWins) {
+		result.textContent = "The machines have defeated the puny humans!";
+  }
+	else {
+		result.textContent = "Mankind has been saved from the machines... For now.";
+  }
 }
 
-function jogo(){
-
-	for(let i = 0; i < 5; i++){
-		selPc = pcJoga();
-		selJogador = jogJoga();
-		console.log("The machines choose: " + selPc);
-		console.log("The humans choose: " + selJogador);
-		Rodada();
-
-	}
-
-	if (pcWins > jogWins){
-		console.log("The machines have defeated the puny humans!");
-	} else {
-		console.log("Mankind has been saved from the machines... For now.");
-	}
-
+function inputHandler(choice) {
+	jogo(choice)
+	previousChoice = choice;
 }
